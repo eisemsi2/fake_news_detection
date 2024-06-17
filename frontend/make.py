1eb72e70efe2947c9fd9830f4f87c38798076788
eisemsi2 <prajjwalk21@iitk.ac.in>
2024-06-17 15:05:02 +0530

    fixed makefile bug

--- frontend/make.py
+++ frontend/make.py
@@ -4,3 +4,6 @@ def make():
     subprocess.run(["npm", "install"])
     subprocess.run(["npm", "run", "build"])
     subprocess.run(["npm", "start"])
+
+if __name__ == "__main__":
+    make()
--- make.py
+++ make.py
@@ -3,3 +3,6 @@ import subprocess
 def make():
     subprocess.run(["./backend/make.py"])
     subprocess.run(["./frontend/make.py"])
+
+if __name__ == "__main__":
+    make()
