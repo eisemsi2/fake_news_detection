ebb8ab0379f1c3c45eaf15e8f4ef2a6c8819ff59
eisemsi2 <96539950+eisemsi2@users.noreply.github.com>
2024-06-17 18:22:05 +0530

    Update webscrap.js

--- backend/lib/webscrap.js
+++ backend/lib/webscrap.js
@@ -2,6 +2,11 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
 
+/**
+ * Reads trusted URLs from a file and returns them as a Set.
+ * Trusted URLs are expected to be listed line by line in 'trusted.txt'.
+ * @returns {Set} Set of trusted URLs.
+ */
 function TrustedUrls() {
     const trusted = new Set();
     const data = fs.readFileSync('trusted.txt', 'utf8');
@@ -12,6 +17,12 @@ function TrustedUrls() {
     return trusted;
 }
 
+/**
+ * Trims a Google search result URL to extract the actual URL.
+ * If the URL starts with '/url?q=', it extracts and returns the URL after removing unnecessary parts.
+ * @param {string} url - The URL from Google search result.
+ * @returns {string} Trimmed URL.
+*/
 function trimurl(url) {
     if (url.startsWith('/url?q=')) {
         // return url.slice(7);
@@ -27,6 +38,12 @@ function trimurl(url) {
     }
     return url;
 }
+/**
+ * Constructs a search query string from the beginning of an article.
+ * Limits the query to the first 150 characters of the article.
+ * @param {string} article - The article content to construct the search query from.
+ * @returns {string} Search query string.
+ */
 function getSearchQuery(article) {
     let query = "";
     for (let i=0; i<article.length; i++) {
@@ -38,10 +55,19 @@ function getSearchQuery(article) {
     return query;
 
 }
+/**
+ * Returns a user-agent string for HTTP headers to simulate a mobile browser.
+ * @returns {string} User-agent string.
+ */
 function getUserAgent() {
     return "Mozilla/5.0 (Linux; Android 7.0) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Focus/4.1 Chrome/62.0.3029.83 Mobile Safari/537.36"
 }
-
+/**
+ * Retrieves relevant URLs from a Google search based on a search query.
+ * Filters URLs based on trusted domains specified in 'trusted.txt'.
+ * @param {string} searchQuery - The search query derived from an article.
+ * @returns {Array} Array of relevant URLs.
+ */
 async function getSomeLinks(searchQuery) {
     const trusted = TrustedUrls();
     const Query = getSearchQuery(searchQuery);
@@ -82,7 +108,12 @@ async function getSomeLinks(searchQuery) {
         return error;
     }
 }
-
+/**
+ * Retrieves the main article content from a given URL.
+ * Uses Cheerio for HTML parsing to extract paragraphs ('p' tags) as article text.
+ * @param {string} url - The URL of the article to retrieve.
+ * @returns {string} Article content.
+ */
 async function getArtilce(url) {
     try {
         const response = await axios.get(url, {
